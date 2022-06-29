51b1275f170beeaec51f091a7c48d85d49a8de37
Sylvester Wilmott <sylvester.wilmott@gmail.com>
2022-06-29 18:14:42 +0100

    Update regex.js

--- src/js/regex.js
+++ src/js/regex.js
@@ -4,5 +4,4 @@ export const UL_REGEX = /^((\t*[-\*]+) {1,})(.+)?$/gm;
 export const OL_REGEX = /^((\t*[0-9]+)\. {1,})(.+)?$/gm;
 export const CL_REGEX = /^((\t*- ?\[x? ?\]) {1,})(.+)?$/gm;
 export const WORD_REGEX = /\w/g;
-export const NEWLINE_REGEX = /\n/g;
 export const WHITESPACE_REGEX = /\s/g;
